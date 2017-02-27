bbab907ee2f8f07f025dd3348b03fb9bac0f478b
AmIsIAreBe <msyoung@utas.edu.au>
2017-02-27 18:28:08 +1100

    Project added
    First commit with basic asp.net setup with kestrel server ready. so far
    it has all been done with CLI.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(
+                Context =>
+                { 
+                    return Context.Response.WriteAsync("Hello Mike into the beyond");
+                }
+            );
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            //execute host and display it to webbrowser
+            host.Run();
+
+            //Console.WriteLine("Hello World!");
+            //var name = Console.ReadLine();
+            //Console.WriteLine($"Hello {name} welcome to C#");
+        }
+    }
+}
